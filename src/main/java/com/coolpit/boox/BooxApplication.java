d14668d86c4ac42639488d37ed698f6404c7c4cb
tebogo <tebogo.assagaai@xgileit.com>
2022-05-15 14:44:46 +0200

    added project base

---
+++ src/main/java/com/coolpit/boox/BooxApplication.java
@@ -0,0 +1,13 @@
+package com.coolpit.boox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coolpit/boox/BooxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolpit.boox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
